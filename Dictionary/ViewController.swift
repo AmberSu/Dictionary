2917b4dd79ee8b6ff7e7d1145e106906a9ac9042
MacOS <MacOS@MacOSs-MacBook-Air.local>
2017-11-22 15:43:17 +0200

    Dictionary using Core Data

--- Dictionary/ViewController.swift
+++ Dictionary/ViewController.swift
@@ -7,19 +7,91 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var englishTextField: UITextField!
+    
+    @IBOutlet weak var norwegianTextField: UITextField!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBOutlet weak var fetchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        englishTextField.delegate = self
+        norwegianTextField.delegate = self
+        formatButton(saveButton)
+        formatButton(fetchButton)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+     // MARK: UIButton methods
+    
+    @IBAction func saveWordsToDictionary(_ sender: UIButton) {
+        guard let englishWord = englishTextField.text, let norwegianWord =
+        norwegianTextField.text, englishWord != "", norwegianWord != "" else {
+            let alert = UIAlertController(title: "Empty text field", message: "Please type a word into all text fields", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .`default`, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        save(englishWord, norwegianWord)
+        englishTextField.text = ""
+        norwegianTextField.text = ""
     }
-
-
+    
+    @IBAction func fetchDataFromDictionary(_ sender: UIButton) {
+        fetchData()
+    }
+    
+    private func formatButton(_ button: UIButton) {
+        button.contentEdgeInsets.bottom = 24
+        button.contentEdgeInsets.top = 24
+        button.contentEdgeInsets.left = 10
+        button.contentEdgeInsets.right = 10
+        button.layer.cornerRadius = 35
+    }
+    
+     // MARK: Core Data methods
+    
+    func save(_ englishWord: String, _ norwegianWord: String) {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        if let entity =
+            NSEntityDescription.entity(forEntityName: "Word",
+                                       in: managedContext) {
+            let word = NSManagedObject(entity: entity,
+                                       insertInto: managedContext)
+            word.setValue(englishWord, forKey: "english")
+            word.setValue(norwegianWord, forKey: "norwegian")
+        }
+    }
+    
+    func fetchData() {
+        let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Word")
+        
+        do {
+            let words: [Any] = try managedContext.fetch(fetchRequest)
+            for word in words {
+                print(word)
+            }
+        } catch let error as NSError {
+            print("Could not fetch. \(error)")
+        }
+    }
+    
+    // MARK: UITextField methods
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
-
