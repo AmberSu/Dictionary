9eff298ef11792a4bfe5585eac66a1e17e10a324
MacOS <MacOS@MacOSs-MacBook-Air.local>
2017-11-26 17:35:35 +0200

    introduced vocabulary class

--- Dictionary/ViewController.swift
+++ Dictionary/ViewController.swift
@@ -21,6 +21,16 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     var words: [NSManagedObject] = []
     
+    class Vocabulary {
+        var english: String
+        var norwegian: String
+        
+        init(english: String, norwegian: String) {
+            self.english = english
+            self.norwegian = norwegian
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         englishTextField.delegate = self
@@ -30,13 +40,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
     // MARK: UIButton methods
     
     @IBAction func saveWordsToDictionary(_ sender: UIButton) {
-        guard let englishWord = englishTextField.text, let norwegianWord =
-            norwegianTextField.text, englishWord != "", norwegianWord != "" else {
+        guard englishTextField.text != "", norwegianTextField.text != "" else {
                 sendAlert(title: "Empty text field", message: "Please type a word into all text fields")
                 return
         }
-        update(englishWord, norwegianWord)
-        save(englishWord, norwegianWord)
+        let vocabulary = Vocabulary(english: englishTextField.text!, norwegian: norwegianTextField.text!)
+        
+        update(vocabulary)
+        save(vocabulary)
         englishTextField.text = ""
         norwegianTextField.text = ""
     }
@@ -63,15 +74,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return nil
     }
     
-    func save(_ englishWord: String, _ norwegianWord: String) {
+    func save(_ vocabulary: Vocabulary) {
         if let managedContext = setContext(), let entity =
             NSEntityDescription.entity(forEntityName: "Word",
                                        in: managedContext) {
             let word = NSManagedObject(entity: entity,
                                        insertInto: managedContext)
             
-            word.setValue(englishWord, forKey: "english")
-            word.setValue(norwegianWord, forKey: "norwegian")
+            word.setValue(vocabulary.english, forKey: "english")
+            word.setValue(vocabulary.norwegian, forKey: "norwegian")
             
             do {
                 try managedContext.save()
@@ -82,13 +93,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    func update(_ englishWord: String, _ norwegianWord: String) {
+    func update(_ vocabulary: Vocabulary) {
         for word in words {
             if let englishSaved = word.value(forKey: "english") as! String?, let norwegianSaved = word.value(forKey: "norwegian") as! String?, let managedContext = setContext() {
                 
-                if englishSaved == englishWord || norwegianSaved == norwegianWord {
-                    word.setValue(englishWord, forKey: "english")
-                    word.setValue(norwegianWord, forKey: "norwegian")
+                if englishSaved == vocabulary.english || norwegianSaved == vocabulary.norwegian {
+                    word.setValue(vocabulary.english, forKey: "english")
+                    word.setValue(vocabulary.norwegian, forKey: "norwegian")
                     sendAlert(title: "Word Update", message: "This word was updated")
                     
                     do {
